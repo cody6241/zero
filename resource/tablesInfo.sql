d086f1c7c8d62e71f0adb535db1de6846b99d187
cody6241 <tea-88@163.com>
2017-04-05 13:58:28 +0800

    增加数据库脚本

---
+++ resource/tablesInfo.sql
@@ -0,0 +1,15 @@
+create table t_user(
+       user_id int auto_increment primary key,       
+       user_name varchar(30),       
+       credits int,       
+       password varchar(32),       
+       last_visit datetime,       
+       last_ip varchar(23)       
+)engine =innodb;
+
+create table t_login_log(
+       login_log_id int auto_increment primary key,
+       user_id int,       
+       ip varchar(23),       
+       login_datetime datetime       
+)engine=innodb;
